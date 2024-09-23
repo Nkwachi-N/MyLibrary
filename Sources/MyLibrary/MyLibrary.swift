93961581b32df4ee05fef3457cff13a2e131fcf8
Nkwachi <nkwachinwamaghinna@gmail.com>
2024-09-23 07:02:04 +0100

    Initial Commit

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public struct MyLibrary{
+    
+    public init(){}
+    
+    public func printHello(){
+        print("Hello, World!")
+    }
+}
